0d234b500ae4c367dc406910f13fcb538cc7aa66
Gabriel Linhares <joao.linhares@itsstecnologia.com.br>
2021-06-30 16:48:54 -0300

    Atualizações

--- lib/views/content/components/botton_navigator_component.dart
+++ lib/views/content/components/botton_navigator_component.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ifood_flutter_clone/core/theme/app_colors.dart';
+import 'package:ifood_flutter_clone/core/theme/app_icons.dart';
 
 class BottonnavigatorComponent extends StatelessWidget {
   const BottonnavigatorComponent({Key key}) : super(key: key);
@@ -8,9 +9,28 @@ class BottonnavigatorComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: AppColors.primaryColor,
+        color: Colors.blue,
       ),
-      child: Column(),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Column(
+                children: [
+                  AppIcon(AppIcons.homeActive),
+                  Text("Início"),
+                ],
+              ),
+              Text("Busca"),
+              Text("Pedidos"),
+              Text("Perfil")
+            ],
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).padding.bottom,
+          )
+        ],
+      ),
     );
   }
 }
